4c737b4f19096e7019c02ac3288e92a1b8dac5ed
X3ric <93117440+X3ric@users.noreply.github.com>
2023-10-24 08:19:25 +0200

    Delete Source directory

--- Source/Libs/graphene.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAPHENE_H
-#define GRAPHENE_H
-    #include <GL/glew.h>
-    #define GLFW_INCLUDE_NONE
-    #include <GLFW/glfw3.h>
-    #include "utils.h"
-    #include "input.h"
-    #include "window.h"
-#endif // GRAPHENE_H
